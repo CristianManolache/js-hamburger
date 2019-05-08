6a0fbb3e61da74fb2025fcc688ae099dfdf55fbc
Manolache Laurentiu Cristian <mlcristian@tiscali.it>
2019-05-08 15:45:20 +0200

    Finished JS (jQuery) Open and Close Hamburger MENU

---
+++ script.js
@@ -0,0 +1,12 @@
+// Viene fornito l’HTML e il CSS, che dovrete leggere e comprendere e starà a voi scrivere il JS utilizzando jQuery, al fine di avere un hamburger menu simile a quello che trovate sul sito di Boolean.
+//
+// Appare il simbolo delle 3 linee solo quando lo schermo è < 1000px, al click del simbolo appare un div che copre l’intero schermo con i link alle sezioni del sito.
+// Un click sul simbolo X, fa scomparire il div
+// ---------------------APRI----------------
+$('.fa-bars').click(function() {
+    $('.hamburger-menu').show();
+});
+// ------------------------CHIUDI--------------
+$('.close').click(function() {
+  $('.hamburger-menu').hide();
+});
